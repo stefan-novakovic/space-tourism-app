1fd26f7ddffcbb655a6ac476e17342ddcbd896a8
Stefan Novakovic <snovakovic3@gmail.com>
2023-12-07 04:01:37 +0100

    Second commit (added side bar navigation); TODO: refactor code

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,21 +5,51 @@ const initApp = () => {
 
 document.addEventListener("DOMContentLoaded", initApp);
 
-const startApp = () => {
-  const hamburgerBtn = document.getElementById("hamburger-btn");
-  hamburgerBtn.addEventListener("click", (event) => {
-    hamburgerBtn.classList.toggle("hamburger-btn-active");
-    hamburgerBtn.classList.toggle("hamburger-btn-notActive");
-  });
+const startApp = async () => {
+  const dataJSON = await getJSONData();
+  exploreBtnListen();
+  hamburgerBtnListenForMenuOpen(dataJSON);
+  hamburgerBtnListenForMenuClose();
+};
 
+const exploreBtnListen = () => {
   const exploreBtn = document.getElementById("explore");
   const exploreOverlay = document.getElementById("explore-overlay");
+
   exploreBtn.addEventListener("mouseover", (event) => {
     exploreOverlay.classList.remove("explore-hover-overlay-notActive");
     exploreOverlay.classList.add("explore-hover-overlay-active");
   });
+
   exploreBtn.addEventListener("mouseleave", (event) => {
     exploreOverlay.classList.remove("explore-hover-overlay-active");
     exploreOverlay.classList.add("explore-hover-overlay-notActive");
   });
+};
+
+const hamburgerBtnListenForMenuOpen = (dataJSON) => {
+  const hamburgerBtnHome = document.getElementById("hamburger-btn-home");
+  const phoneMenu = document.getElementById("phone-menu");
+
+  hamburgerBtnHome.addEventListener("click", (event) => {
+    phoneMenu.classList.remove("phoneMenuHide");
+    phoneMenu.classList.add("phoneMenuShow");
+  });
+  console.log(dataJSON);
+};
+
+const hamburgerBtnListenForMenuClose = () => {
+  const hamburgerBtnMenu = document.getElementById("hamburger-btn-menu");
+  const phoneMenu = document.getElementById("phone-menu");
+
+  hamburgerBtnMenu.addEventListener("click", (event) => {
+    phoneMenu.classList.remove("phoneMenuShow");
+    phoneMenu.classList.add("phoneMenuHide");
+  });
+};
+
+const getJSONData = async () => {
+  const data = await fetch("data.json");
+  const dataJSON = await data.json();
+  return dataJSON;
 };
