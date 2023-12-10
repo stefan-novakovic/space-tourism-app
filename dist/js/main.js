59c3493606251e9698b4146cf037bfb530eb637f
Stefan Novakovic <snovakovic3@gmail.com>
2023-12-10 16:47:10 +0100

    Added crew page; added shell structure to crew page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,6 @@
+import { exploreBtnListen, homeHamburgerBtnListen } from "./homePage.js";
+import { crewHamburgerBtnListen } from "./crewPage.js";
+
 const initApp = () => {
   console.log("DOM Content Loaded!");
   startApp();
@@ -8,38 +11,8 @@ document.addEventListener("DOMContentLoaded", initApp);
 const startApp = async () => {
   const dataJSON = await getJSONData();
   exploreBtnListen();
-  hamburgerBtnListen(dataJSON);
-};
-
-const exploreBtnListen = () => {
-  const exploreBtn = document.getElementById("explore-btn");
-  const exploreBtnOverlay = document.getElementById("explore-btn-overlay");
-
-  exploreBtn.addEventListener("mouseover", (event) => {
-    exploreBtnOverlay.classList.remove("overlay-not-active");
-    exploreBtnOverlay.classList.add("overlay-active");
-  });
-
-  exploreBtn.addEventListener("mouseleave", (event) => {
-    exploreBtnOverlay.classList.remove("overlay-active");
-    exploreBtnOverlay.classList.add("overlay-not-active");
-  });
-};
-
-const hamburgerBtnListen = (dataJSON) => {
-  const hamburgerBtnOpenMenu = document.getElementById("hamburger-btn-open");
-  const hamburgerBtnCloseMenu = document.getElementById("hamburger-btn-close");
-  const phoneMenu = document.getElementById("phone-menu");
-
-  hamburgerBtnOpenMenu.addEventListener("click", (event) => {
-    phoneMenu.classList.remove("phoneMenuHide");
-    phoneMenu.classList.add("phoneMenuShow");
-  });
-
-  hamburgerBtnCloseMenu.addEventListener("click", (event) => {
-    phoneMenu.classList.remove("phoneMenuShow");
-    phoneMenu.classList.add("phoneMenuHide");
-  });
+  homeHamburgerBtnListen(dataJSON);
+  crewHamburgerBtnListen(dataJSON);
 };
 
 const getJSONData = async () => {
