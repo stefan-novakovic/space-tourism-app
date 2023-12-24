f5e9c11ea544b30c6efb87a6c6103529515e850d
Stefan Novakovic <snovakovic3@gmail.com>
2023-12-24 18:23:15 +0100

    Phone menu functionality added; Wiring menus with pages done; animations done

--- dist/js/main.js
+++ dist/js/main.js
@@ -36,146 +36,43 @@ const getJSONData = async () => {
   return dataJSON;
 };
 
-let counter = 0;
 let inProgress = false;
 const navMenuMarkerPositionSwitchListen = () => {
   const marker = document.getElementById("menu-marker");
   const listItem = document.querySelectorAll(".header .nav .ul .li");
+  const listItemPhone = document.querySelectorAll("main .nav-phone .ul .li");
 
   for (let i = 0; i < listItem.length; i++) {
     listItem[i].addEventListener("click", (event) => {
       if (!inProgress) {
         inProgress = true;
 
-        counter += 1;
         marker.style.left = listItem[i].offsetLeft + "px";
         marker.style.width = listItem[i].offsetWidth + "px";
 
-        if (counter % 2 !== 0) {
-          document.getElementById("home-page").style.transform =
-            "translateX(102%)";
-        } else {
-          document.getElementById("home-page").style.transform =
-            "translateX(-102%)";
-        }
-        document.getElementById("home-page").style.transition =
-          "all 1s ease-in-out";
+        pageSwitch(i);
         setTimeout(() => {
-          if (counter % 2 !== 0) {
-            document.getElementById("home-page").style.transform =
-              "translateY(102%)";
-          } else {
-            document.getElementById("home-page").style.transform =
-              "translateY(-102%)";
-          }
-          document.getElementById("home-page").style.transition = "none";
-          document.getElementById("home-page").style.display = "none";
-        }, 1001);
+          inProgress = false;
+        }, 1801);
+      }
+    });
+  }
 
-        if (counter % 2 !== 0) {
-          document.getElementById("destination-page").style.transform =
-            "translateX(102%)";
-        } else {
-          document.getElementById("destination-page").style.transform =
-            "translateX(-102%)";
-        }
-        document.getElementById("destination-page").style.transition =
-          "all 1s ease-in-out";
-        setTimeout(() => {
-          if (counter % 2 !== 0) {
-            document.getElementById("destination-page").style.transform =
-              "translateY(102%)";
-          } else {
-            document.getElementById("destination-page").style.transform =
-              "translateY(-102%)";
-          }
-          document.getElementById("destination-page").style.transition = "none";
-          document.getElementById("destination-page").style.display = "none";
-        }, 1001);
+  for (let i = 0; i < listItemPhone.length; i++) {
+    listItemPhone[i].addEventListener("click", (event) => {
+      setTimeout(() => {
+        const phoneSidebarMenu = document.getElementById("phone-sidebar-menu");
+        phoneSidebarMenu.classList.remove("phoneMenuShow");
+        phoneSidebarMenu.classList.add("phoneMenuHide");
+      }, 275);
 
-        if (counter % 2 !== 0) {
-          document.getElementById("crew-page").style.transform =
-            "translateX(102%)";
-        } else {
-          document.getElementById("crew-page").style.transform =
-            "translateX(-102%)";
-        }
-        document.getElementById("crew-page").style.transition =
-          "all 1s ease-in-out";
-        setTimeout(() => {
-          if (counter % 2 !== 0) {
-            document.getElementById("crew-page").style.transform =
-              "translateY(102%)";
-          } else {
-            document.getElementById("crew-page").style.transform =
-              "translateY(-102%)";
-          }
-          document.getElementById("crew-page").style.transition = "none";
-          document.getElementById("crew-page").style.display = "none";
-        }, 1001);
+      if (!inProgress) {
+        inProgress = true;
 
-        if (counter % 2 !== 0) {
-          document.getElementById("technology-page").style.transform =
-            "translateX(102%)";
-        } else {
-          document.getElementById("technology-page").style.transform =
-            "translateX(-102%)";
-        }
-        document.getElementById("technology-page").style.transition =
-          "all 1s ease-in-out";
-        setTimeout(() => {
-          if (counter % 2 !== 0) {
-            document.getElementById("technology-page").style.transform =
-              "translateY(102%)";
-          } else {
-            document.getElementById("technology-page").style.transform =
-              "translateY(-102%)";
-          }
-          document.getElementById("technology-page").style.transition = "none";
-          document.getElementById("technology-page").style.display = "none";
-        }, 1001);
+        marker.style.left = listItemPhone[i].offsetLeft + "px";
+        marker.style.width = listItemPhone[i].offsetWidth + "px";
 
-        if (i === 0) {
-          setTimeout(() => {
-            document.getElementById("home-page").style.display = "flex";
-            setTimeout(() => {
-              document.getElementById("home-page").style.transform =
-                "translateX(0%)";
-              document.getElementById("home-page").style.transition =
-                "all 0.75s ease-in-out";
-            }, 48);
-          }, 1002);
-        } else if (i === 1) {
-          setTimeout(() => {
-            document.getElementById("destination-page").style.display = "flex";
-            setTimeout(() => {
-              document.getElementById("destination-page").style.transform =
-                "translateX(0%)";
-              document.getElementById("destination-page").style.transition =
-                "all 0.75s ease-in-out";
-            }, 48);
-          }, 1002);
-        } else if (i === 2) {
-          setTimeout(() => {
-            document.getElementById("crew-page").style.display = "flex";
-            setTimeout(() => {
-              document.getElementById("crew-page").style.transform =
-                "translateX(0%)";
-              document.getElementById("crew-page").style.transition =
-                "all 0.75s ease-in-out";
-            }, 48);
-          }, 1002);
-        } else if (i === 3) {
-          setTimeout(() => {
-            document.getElementById("technology-page").style.display = "flex";
-            setTimeout(() => {
-              document.getElementById("technology-page").style.transform =
-                "translateX(0%)";
-              document.getElementById("technology-page").style.transition =
-                "all 0.75s ease-in-out";
-            }, 48);
-          }, 1002);
-        }
+        pageSwitch(i);
         setTimeout(() => {
           inProgress = false;
         }, 1801);
@@ -198,4 +95,166 @@ const hamburgerBtnListen = () => {
     phoneSidebarMenu.classList.remove("phoneMenuShow");
     phoneSidebarMenu.classList.add("phoneMenuHide");
   });
+};
+
+let counter = 0;
+const pageSwitch = (index) => {
+  const deviceScreenWidth768px = window.matchMedia("(min-width: 768px)");
+  counter += 1;
+
+  if (deviceScreenWidth768px.matches) {
+    if (counter % 2 !== 0) {
+      document.getElementById("home-page").style.transform = "translateX(102%)";
+    } else {
+      document.getElementById("home-page").style.transform =
+        "translateX(-102%)";
+    }
+  } else {
+    document.getElementById("home-page").style.transform = "translateX(-102%)";
+  }
+  document.getElementById("home-page").style.transition = "all 1s ease-in";
+  setTimeout(() => {
+    if (deviceScreenWidth768px.matches) {
+      if (counter % 2 !== 0) {
+        document.getElementById("home-page").style.transform =
+          "translateY(102%)";
+      } else {
+        document.getElementById("home-page").style.transform =
+          "translateY(-102%)";
+      }
+    } else {
+      document.getElementById("home-page").style.transform = "translateY(102%)";
+    }
+    document.getElementById("home-page").style.transition = "none";
+    document.getElementById("home-page").style.display = "none";
+  }, 1001);
+
+  if (deviceScreenWidth768px.matches) {
+    if (counter % 2 !== 0) {
+      document.getElementById("destination-page").style.transform =
+        "translateX(102%)";
+    } else {
+      document.getElementById("destination-page").style.transform =
+        "translateX(-102%)";
+    }
+  } else {
+    document.getElementById("destination-page").style.transform =
+      "translateX(-102%)";
+  }
+  document.getElementById("destination-page").style.transition =
+    "all 1s ease-in";
+  setTimeout(() => {
+    if (deviceScreenWidth768px.matches) {
+      if (counter % 2 !== 0) {
+        document.getElementById("destination-page").style.transform =
+          "translateY(102%)";
+      } else {
+        document.getElementById("destination-page").style.transform =
+          "translateY(-102%)";
+      }
+    } else {
+      document.getElementById("destination-page").style.transform =
+        "translateY(102%)";
+    }
+    document.getElementById("destination-page").style.transition = "none";
+    document.getElementById("destination-page").style.display = "none";
+  }, 1001);
+
+  if (deviceScreenWidth768px.matches) {
+    if (counter % 2 !== 0) {
+      document.getElementById("crew-page").style.transform = "translateX(102%)";
+    } else {
+      document.getElementById("crew-page").style.transform =
+        "translateX(-102%)";
+    }
+  } else {
+    document.getElementById("crew-page").style.transform = "translateX(-102%)";
+  }
+  document.getElementById("crew-page").style.transition = "all 1s ease-in";
+  setTimeout(() => {
+    if (deviceScreenWidth768px.matches) {
+      if (counter % 2 !== 0) {
+        document.getElementById("crew-page").style.transform =
+          "translateY(102%)";
+      } else {
+        document.getElementById("crew-page").style.transform =
+          "translateY(-102%)";
+      }
+    } else {
+      document.getElementById("crew-page").style.transform = "translateY(102%)";
+    }
+    document.getElementById("crew-page").style.transition = "none";
+    document.getElementById("crew-page").style.display = "none";
+  }, 1001);
+
+  if (deviceScreenWidth768px.matches) {
+    if (counter % 2 !== 0) {
+      document.getElementById("technology-page").style.transform =
+        "translateX(102%)";
+    } else {
+      document.getElementById("technology-page").style.transform =
+        "translateX(-102%)";
+    }
+  } else {
+    document.getElementById("technology-page").style.transform =
+      "translateX(-102%)";
+  }
+  document.getElementById("technology-page").style.transition =
+    "all 1s ease-in";
+  setTimeout(() => {
+    if (deviceScreenWidth768px.matches) {
+      if (counter % 2 !== 0) {
+        document.getElementById("technology-page").style.transform =
+          "translateY(102%)";
+      } else {
+        document.getElementById("technology-page").style.transform =
+          "translateY(-102%)";
+      }
+    } else {
+      document.getElementById("technology-page").style.transform =
+        "translateY(102%)";
+    }
+    document.getElementById("technology-page").style.transition = "none";
+    document.getElementById("technology-page").style.display = "none";
+  }, 1001);
+
+  if (index === 0) {
+    setTimeout(() => {
+      document.getElementById("home-page").style.display = "flex";
+      setTimeout(() => {
+        document.getElementById("home-page").style.transform = "translateX(0%)";
+        document.getElementById("home-page").style.transition =
+          "all 0.75s ease-out";
+      }, 48);
+    }, 1002);
+  } else if (index === 1) {
+    setTimeout(() => {
+      document.getElementById("destination-page").style.display = "flex";
+      setTimeout(() => {
+        document.getElementById("destination-page").style.transform =
+          "translateX(0%)";
+        document.getElementById("destination-page").style.transition =
+          "all 0.75s ease-out";
+      }, 48);
+    }, 1002);
+  } else if (index === 2) {
+    setTimeout(() => {
+      document.getElementById("crew-page").style.display = "flex";
+      setTimeout(() => {
+        document.getElementById("crew-page").style.transform = "translateX(0%)";
+        document.getElementById("crew-page").style.transition =
+          "all 0.75s ease-out";
+      }, 48);
+    }, 1002);
+  } else if (index === 3) {
+    setTimeout(() => {
+      document.getElementById("technology-page").style.display = "flex";
+      setTimeout(() => {
+        document.getElementById("technology-page").style.transform =
+          "translateX(0%)";
+        document.getElementById("technology-page").style.transition =
+          "all 0.75s ease-out";
+      }, 48);
+    }, 1002);
+  }
 };
